e06af48fb1e32fb5db55dbe8e562839522b2bdf4
Carlos Valverde <studio@carlos-valverde.com>
2023-11-12 22:19:39 +0100

    --wip--

--- header.php
+++ header.php
@@ -39,11 +39,11 @@
                 </svg>
             </a>
         </div>
-        <nav class="navbar navbar-expand-lg col">
+        <nav class="navbar navbar-expand-lg col justify-content-end">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item me-5">
                             <a class="nav-link" href="/editions">Edições</a>
